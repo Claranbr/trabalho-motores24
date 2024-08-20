930413e6273938b9bda4aefa97af3551800c8080
Claranbr <clara.nobre@escolar.ifrn.edu.br>
2024-08-20 16:30:04 -0300

    fase1

---
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using Vector3 = UnityEngine.Vector3;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int velocidade = 10;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("star");
+        TryGetComponent(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new UnityEngine.Vector3(x,0,y);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        
+        if (transform.position.y <= -10)
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+        
+        
+    }
+}
